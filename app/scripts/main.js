514dfc586379880acf89e96e66edfb8a2f5d0ed5
sansimian <aterruso@gmail.com>
2016-11-17 20:27:27 -0500

    Updated Lighting. Experimented with using a bunch of different lighting and textures.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,14 +29,13 @@ var controls = new THREE.OrbitControls( camera, renderer.domElement);
 /* CREATE THE SHAPE */
 var geometry = new THREE.BoxGeometry(1.2, 1.2, 1.2);
 var cubeMaterials = [
-  new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide}), /* RIGHT SIDE */
-
-  // new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../images/1.jpg'), side: THREE.DoubleSide}), /* RIGHT SIDE */
-  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../images/2.jpg'), side: THREE.DoubleSide}), /* LEFT SIDE */
-  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../images/3.jpg'), side: THREE.DoubleSide}), /* TOP SIDE */
-  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../images/4.jpg'), side: THREE.DoubleSide}), /* BOTTOM SIDE */
-  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../images/5.jpg'), side: THREE.DoubleSide}), /* FRONT SIDE */
-  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../images/6.jpg'), side: THREE.DoubleSide})  /* BACK SIDE */
+  // new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide}), /* RIGHT SIDE */
+  new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('../images/1.jpg'), side: THREE.DoubleSide}), /* RIGHT SIDE */
+  new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('../images/2.jpg'), side: THREE.DoubleSide}), /* LEFT SIDE */
+  new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('../images/3.jpg'), side: THREE.DoubleSide}), /* TOP SIDE */
+  new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('../images/4.jpg'), side: THREE.DoubleSide}), /* BOTTOM SIDE */
+  new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('../images/5.jpg'), side: THREE.DoubleSide}), /* FRONT SIDE */
+  new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('../images/6.jpg'), side: THREE.DoubleSide})  /* BACK SIDE */
 ];
 
 /* CREATE A MATERIAL COLOR OR IMAGE TEXTURE */
@@ -50,11 +49,49 @@ var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 camera.position.z = 3;
 
+/* Ambient Light */
+var ambientLight = new THREE.AmbientLight (0xFFFFFF, 0.5);
+scene.add( ambientLight );
+
+var light1 = new THREE.PointLight( 0xFF0040, 6, 50 );
+scene.add(light1);
+
+
+var light2 = new THREE.PointLight( 0xFF40FF, 5, 50 );
+scene.add(light2);
+
+
+var light3 = new THREE.PointLight( 0x80FF80, 8, 50 );
+scene.add(light3);
+
+
+var directionalLight - new THREE.DirectionalLight( 0xFFFFFF, 1 );
+directionalLight.position.set(0, 1, 0);
+scene.add(directionalLight);
+
+var spotLight = mew THREE.SportLight(0XFF45F6, 25);
+spotLight.position.set(0, 3, 0);
+scene.add(spotLight);
+
 
 /* SCENE LOGIC */
 var update = function() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.005;
+
+  var time = Date.now() * 0.0005;
+
+  light1.position.x = Math.sin(time * 0.7) * 30;
+  light1.position.y = Math.cos(time * 0.5) * 40;
+  light1.position.z = Math.cos(time * 0.3) * 30;
+
+  light2.position.x = Math.sin(time * 0.7) * 30;
+  light2.position.y = Math.cos(time * 0.5) * 40;
+  light2.position.z = Math.cos(time * 0.3) * 30;
+
+  light3.position.x = Math.sin(time * 0.7) * 30;
+  light3.position.y = Math.cos(time * 0.5) * 40;
+  light3.position.z = Math.cos(time * 0.3) * 30;
 };
 
 /* DRAW SCENE */
